8f962eb10a1971e285d9364577a20598f4d286e7
Fumble Pin <memoo44085@gmail.com>
2025-02-01 22:45:51 +0530

    Update icons-cdn.js

--- src/icons-cdn.js
+++ src/icons-cdn.js
@@ -1,1 +1,30 @@
-123
+// icons-cdn.js
+(function() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    
+    // Add CSS for the icons
+    style.innerHTML = `
+        .i-arrow-up, .i-arrow-down, .i-arrow-left, .i-arrow-right {
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+            background-size: contain;
+        }
+        .i-arrow-up {
+            background-image: url('https://your-cdn-link/arrow-up.svg');
+        }
+        .i-arrow-down {
+            background-image: url('https://your-cdn-link/arrow-down.svg');
+        }
+        .i-arrow-left {
+            background-image: url('https://your-cdn-link/arrow-left.svg');
+        }
+        .i-arrow-right {
+            background-image: url('https://your-cdn-link/arrow-right.svg');
+        }
+    `;
+    
+    document.head.appendChild(style);
+})();
+
