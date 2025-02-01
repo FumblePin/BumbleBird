a47c9444325683c45e3f08c7d98e447b26c7c183
Fumble Pin <memoo44085@gmail.com>
2025-02-01 23:41:28 +0530

    Update icons-cdn.js

--- src/icons-cdn.js
+++ src/icons-cdn.js
@@ -1,37 +1,13 @@
-// icons-cdn.js
-(function() {
-    const style = document.createElement('style');
-    style.type = 'text/css';
+document.querySelectorAll("i").forEach(icon => {
+    let classList = Array.from(icon.classList);
+    let iconClass = classList.find(cls => cls.startsWith("i-"));
     
-    // Add CSS for the icons
-    style.innerHTML = `
-        .i-arrow-up, .i-arrow-down, .i-arrow-left, .i-arrow-right {
-            display: inline-block;
-            width: var(--icon-size, 24px); /* Default size */
-            height: var(--icon-size, 24px); /* Default size */
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: center;
-            fill: var(--icon-color, black); /* Default color */
-        }
-        .i-arrow-up {
-            background-image: url('https://your-cdn-link/arrow-up.svg');
-        }
-        .i-arrow-down {
-            background-image: url('https://your-cdn-link/arrow-down.svg');
-        }
-        .i-arrow-left {
-            background-image: url('https://your-cdn-link/arrow-left.svg');
-        }
-        .i-arrow-right {
-            background-image: url('https://your-cdn-link/arrow-right.svg');
-        }
-    `;
-    
-    document.head.appendChild(style);
-    
-    // Function to change the size and color of icons
-    window.changeIconStyles = function(size, color) {
+    if (iconClass) {
+        let iconName = iconClass.replace("i-", "");
+        icon.style.backgroundImage = `url('https://your-cdn-link/Icon/${iconName}.svg')`;
+    }
+});
+
         document.documentElement.style.setProperty('--icon-size', size);
         document.documentElement.style.setProperty('--icon-color', color);
     };
