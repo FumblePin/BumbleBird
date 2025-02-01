b639111dfe2c9f09c8b483a9f106e8d5a19b499b
Fumble Pin <memoo44085@gmail.com>
2025-02-01 23:16:36 +0530

    Update icons-cdn.js

--- src/icons-cdn.js
+++ src/icons-cdn.js
@@ -7,9 +7,12 @@
     style.innerHTML = `
         .i-arrow-up, .i-arrow-down, .i-arrow-left, .i-arrow-right {
             display: inline-block;
-            width: 24px;
-            height: 24px;
+            width: var(--icon-size, 24px); /* Default size */
+            height: var(--icon-size, 24px); /* Default size */
             background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+            fill: var(--icon-color, black); /* Default color */
         }
         .i-arrow-up {
             background-image: url('https://your-cdn-link/arrow-up.svg');
@@ -26,5 +29,11 @@
     `;
     
     document.head.appendChild(style);
+    
+    // Function to change the size and color of icons
+    window.changeIconStyles = function(size, color) {
+        document.documentElement.style.setProperty('--icon-size', size);
+        document.documentElement.style.setProperty('--icon-color', color);
+    };
 })();
 
