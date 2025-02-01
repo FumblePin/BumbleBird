799e1851e91f9a2bc1f974e21bd8b80486aa08c5
Fumble Pin <memoo44085@gmail.com>
2025-02-01 23:47:29 +0530

    Update icons-cdn.js

--- src/icons-cdn.js
+++ src/icons-cdn.js
@@ -4,7 +4,7 @@ document.querySelectorAll("i").forEach(icon => {
     
     if (iconClass) {
         let iconName = iconClass.replace("i-", "");
-        icon.style.backgroundImage = `url('https://cdn.jsdelivr.net/gh/FumblePin/BumbleBird/Icon/${iconName}.svg')`;
+        icon.style.backgroundImage = `url('https://cdn.jsdelivr.net/gh/FumblePin/BumbleBird@0376a93099d0f62baf3cdfc07f5a247c6b597b34/Icon/${iconName}.svg')`;
     }
 });
 
@@ -12,6 +12,4 @@ document.querySelectorAll("i").forEach(icon => {
         document.documentElement.style.setProperty('--icon-color', color);
     };
 })();
-
-https://cdn.jsdelivr.net/gh/FumblePin/BumbleBird@bf3178cf53fca6cfdd58a4d2377fd1893bf2c93d/css/icon.css
 
