d77d5fe5f9d784dbbb1ebb78a873ba277985aee6
Fumble Pin <memoo44085@gmail.com>
2025-02-02 23:54:16 +0530

    Delete src directory

--- src/generate.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = './Icon/Arrows';
-
-fs.readdirSync(path).forEach(file => {
-    console.log(`Generated <i> tag: <i class="i-${file.replace('.svg', '')}"></i>`);
-});
